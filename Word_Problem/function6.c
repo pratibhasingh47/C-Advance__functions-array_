5e6ee28d2e6a9528938dc02edfdfd7d98bc6a5e6
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-13 00:46:18 +0530

    update.c

--- Word_Problem/function6.c
+++ Word_Problem/function6.c
@@ -1,3 +1,6 @@
+// Write a function to calculate the factorial of a number. Call this function from main() function. Pass the value of number whose factorial needs to be calculated, as an argument to this function. Function should calculate the factorial and then return the calculated factorial. In main() function receive this value in a variable and then print the value of this variable .
+
+
 #include <stdio.h>
 int factorial(int x);
 int main()
