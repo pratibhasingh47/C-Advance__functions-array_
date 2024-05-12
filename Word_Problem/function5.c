3c05163a6355716876d30fd78baf8862290147c2
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-13 00:43:29 +0530

    update.c

--- Word_Problem/function5.c
+++ Word_Problem/function5.c
@@ -1,19 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 void Nno(int n);
 int main()
 {
     int n;
     printf("Enter the value\n");
-    scanf("%d",&n);
+    scanf("%d", &n);
     Nno(n);
 }
 void Nno(int n)
 {
     int i;
-    for ( i = 1; i<=n; i++)
+    for (i = 1; i <= n; i++)
     {
-        if(i%2!=0)
-        {printf("%d\n",i);}
+        if (i % 2 != 0)
+        {
+            printf("%d\n", i);
+        }
     }
-    
 }
--- Word_Problem/function6.c
+++ Word_Problem/function6.c
@@ -1,21 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 int factorial(int x);
 int main()
 {
     int x;
     int i;
     printf("Enter the value\n");
-    scanf("%d",&x);
+    scanf("%d", &x);
     int ft;
-    ft=factorial(x);
-    printf("%d",ft);
+    ft = factorial(x);
+    printf("%d", ft);
 }
 int factorial(int x)
 {
-    int i,f=1;
-    for ( i = 1; i <= x; i++)
+    int i, f = 1;
+    for (i = 1; i <= x; i++)
     {
-        f=f*i;
+        f = f * i;
     }
     return f;
 }
