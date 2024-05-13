036fb49fa8f3c4db6b58f7fb82f36ac17dd743ba
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-14 00:36:36 +0530

    c

--- Word_Problem/function7.c
+++ Word_Problem/function7.c
@@ -1,3 +1,6 @@
+// Write a function to calculate the number of combinations one can make from n items and r selected at a time. Write one function to calculate the combinations. Now call this function from the main() function. Pass the value of ‘n’ and ‘r’ as function arguments. Receive these values in function parameters. Now calculate the result. Function should return this result. Receive the result in a variable in main() function and print it.
+
+
 #include <stdio.h>
 int factorial(int n);
 float combination(int x, int y, int z);
--- Word_Problem/function9.c
+++ Word_Problem/function9.c
@@ -1,3 +1,6 @@
+//Write a function to check whether a given number contains a given digit or not. Write a function and pass the ‘number’ and the ‘digit’ as function arguments. In the function definition, receive the value of arguments in formal parameters. Now in the function, check whether a given number contains a digit or not. Return the result. For example, the function will return ‘1’ if the digit is present, otherwise it will return 0. Now in main() function receive this result and print message accordingly. If function return ‘1’ then it should print the message DIGIT IS PRESENT IN THE NUMBER. Otherwise it should print DIGIT IS NOT PRESENT IN THE NUMBER.
+
+
 #include <stdio.h>
 int digit(int b, int y);
 int main()
