54e8a06ebe56f903f6887a1088cb32473f2f2325
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-14 00:37:04 +0530

    update.c

--- Word_Problem/function10.c
+++ Word_Problem/function10.c
@@ -1,5 +1,4 @@
-//Write a function to print all prime factors of a given number. For example, if the number is 36 then your result should be 2, 2, 3, 3. Write the function as pass the ‘number’ as an argument. In the function definition, receive this value in function parameters. Find all the prime factors and print them inside the function. Function need not to return anything.
-
+// Write a function to print all prime factors of a given number. For example, if the number is 36 then your result should be 2, 2, 3, 3. Write the function as pass the ‘number’ as an argument. In the function definition, receive this value in function parameters. Find all the prime factors and print them inside the function. Function need not to return anything.
 
 #include <stdio.h>
 int main()
--- Word_Problem/function8.c
+++ Word_Problem/function8.c
@@ -1,6 +1,5 @@
 // Write a function to calculate the number of arrangements one can make from n items and r selected at a time. Same as question 8.
 
-
 #include <stdio.h>
 int factorial(int n);
 float arrangement(int x, int z);
