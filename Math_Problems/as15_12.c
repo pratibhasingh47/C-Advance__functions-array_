60d4d5c5536ffb14d0b55d53258696926d57926a
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-15 10:28:16 +0530

    update.c

---
+++ Math_Problems/as15_12.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i=2;int x;int y;
+    printf("Enter the numbers :\n");
+    scanf("%d\n%d",&x,&y);
+    int k;
+    k=x*y;
+    while (k>0)
+    {
+        if (x%k==0 && y%k==0)
+        {
+            printf("HCF is %d",k);
+            break;
+        }
+        else{k--;}
+    }
+}
