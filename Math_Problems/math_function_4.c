468c9a511669f73ef256bcf6709c17b93d6f694a
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-17 00:31:58 +0530

    c

--- Math_Problems/math_function_4.c
+++ Math_Problems/math_function_4.c
@@ -1,47 +1,48 @@
-#include<stdio.h>
+#include <stdio.h>
 int prime(int a);
 int main()
 {
-     int n,z;
-     printf("Enter the no. :");
-     scanf("%d",&n);
-     while (n>0)
-     {
-        z=prime(n);
-        if (z==1)
+    int n, z;
+    printf("Enter the no. :");
+    scanf("%d", &n);
+    while (n > 0)
+    {
+        z = prime(n);
+        if (z == 1)
         {
-            printf("%d",n);
+            printf("%d", n);
             break;
         }
-        else if (z==0)
+        else if (z == 0)
         {
             n++;
         }
-     }
+    }
 }
 int prime(int a)
 {
-    int i=2;int k;
-    if(a>0)
+    int i = 2;
+    int k;
+    if (a > 0)
     {
-        for (i = 2;i>0; i++)
+        for (i = 2; i > 0; i++)
         {
-            if (a%i==0)
+            if (a % i == 0)
             {
-                k=a/i;
-                if (k!=1)
+                k = a / i;
+                if (k != 1)
                 {
-                  //  printf("NOT PRIME ");
-                  //  break;
-                  return 0;
+                    //  printf("NOT PRIME ");
+                    //  break;
+                    return 0;
                 }
-                else if (a==i)
+                else if (a == i)
                 {
-                    //printf("%d\n",a);
-                   // break;
+                    // printf("%d\n",a);
+                    // break;
                     return 1;
-                }       
+                }
             }
-        }    
+        }
     }
 }
