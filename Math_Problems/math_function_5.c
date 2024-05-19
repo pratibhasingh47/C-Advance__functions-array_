d50529b93641eef6f7e7e2c68b4e51b85de572e4
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-19 16:28:00 +0530

    update.c

---
+++ Math_Problems/math_function_5.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int prime(int a);
+int main()
+{
+    int n;
+   printf("Enter the values :");
+   scanf("%d",&n);
+    int i;
+    while (n>0)
+    {
+        for ( i = 2; i > 0; i++)
+        {
+            int z;
+            z=prime(i);
+            if (z==1)
+            {
+                printf("%d\n",i);
+                n--;
+            }
+            else if (n<0)
+            {
+                break;
+            }}} 
+}
+int prime(int a)
+{
+    int i=2;int k;
+    if(a>0)
+    {
+        for (i = 2;i>0; i++)
+        {
+            if (a%i==0)
+            {
+                k=a/i;
+                if (k!=1)
+                {
+                   // printf("NOT PRIME ");
+                    break;return 0;
+                }
+                else if (a==i)
+                {
+                  //  printf("%d\n",a);
+                  // break;
+                    return 1;
+                }}}}
+}
---
+++ Math_Problems/math_function_6.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int prime(int a);
+int main()
+{
+    int n,v;
+   printf("Enter the values :");
+   scanf("%d\n%d",&n,&v);
+    int i;
+    for ( i = n; i <= v && i>= n; i++)
+    {
+        int z;
+        z=prime(i);
+    }
+    
+}
+int prime(int a)
+{
+    int i=2;int k;
+    if(a>0)
+    {
+        for (i = 2;i>0; i++)
+        {
+            if (a%i==0)
+            {
+                k=a/i;
+                if (k!=1)
+                {
+                  //  printf("NOT PRIME ");
+                    break;return 0;
+                }
+                else if (a==i)
+                {
+                    printf("%d\n",a);
+                    break;
+                    return 1;
+                }       
+            }
+        }    
+    }
+}
