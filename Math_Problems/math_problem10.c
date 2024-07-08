71b80f07726bec2d90f65faef766d16db525c038
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-07-08 22:13:30 +0530

    math_problem

--- Math_Problems/math_problem10.c
+++ Math_Problems/math_problem10.c
@@ -1,28 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
 int factorial(int x);
 int main()
 {
-    int a=0; int b,c,p;
-    int n=5;
+    int a = 0;
+    int b, c, p;
+    int n = 5;
     printf("Sum of the series will be :");
-    for ( p = 1; p <= n; p++)
+    for (p = 1; p <= n; p++)
     {
-        b= (factorial(p))/p;
-        c=a+b;
-        a=c;
-        if (p==n)
+        b = (factorial(p)) / p;
+        c = a + b;
+        a = c;
+        if (p == n)
         {
-            printf("%d",c);
+            printf("%d", c);
             break;
         }
     }
 }
 int factorial(int x)
 {
-    int i,f=1;
-    for ( i = 1; i <= x; i++)
+    int i, f = 1;
+    for (i = 1; i <= x; i++)
     {
-        f=f*i;
+        f = f * i;
     }
     return f;
 }
