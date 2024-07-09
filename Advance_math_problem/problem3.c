d6c3a8ed25dd85a94f1739fedb57e0098b6f1657
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-07-09 22:25:22 +0530

    problem

--- Advance_math_problem/problem3.c
+++ Advance_math_problem/problem3.c
@@ -1,18 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int i,n;
-    int k=0;
-    printf("Enter the value :");
-    scanf("%d",&n);
-   for ( i = 1; i <=n; i++)
+   int i, n;
+   int k = 0;
+   printf("Enter the value :");
+   scanf("%d", &n);
+   for (i = 1; i <= n; i++)
    {
-      if (i%2!=0)
+      if (i % 2 != 0)
       {
-         k=(k+ i);
-         if (i==n)
+         k = (k + i);
+         if (i == n)
          {
-            printf("%d",k);
+            printf("%d", k);
          }
-      } 
-}}
+      }
+   }
+}
