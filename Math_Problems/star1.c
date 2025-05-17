e0e03f55f58ef28fd6811d41b859275c7c59ef88
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2025-05-17 21:46:33 +0530

    problems

---
+++ Math_Problems/star1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n=5;int i; int j;
+    for ( i = 1; i <= n; i++)
+    {
+       for ( j = 1; j <= n; j++)
+       {
+        printf("*");
+       }
+       printf("\n");
+    }
+    
+}
