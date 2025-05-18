d19c56014200fe1f1766df31d2f4d8e5df38a7eb
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2025-05-18 21:56:40 +0530

    problems

---
+++ Math_Problems/starpy.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n=5,j,i;
+    for ( i = n; i <= n; i--)
+    {
+        for ( j = 1; j <= n-i; j++)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= (2*i)-1; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+        if (i==0)
+        {
+            break;
+        }
+        
+    }
+    
+}
