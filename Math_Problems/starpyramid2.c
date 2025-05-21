57d04eb32fa7bcc322ff95ff532835f80516e58d
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2025-05-21 22:13:06 +0530

    problems

---
+++ Math_Problems/starpyramid2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n=5,i,j;
+    for ( i = 1; i <= n; i++)
+    {
+        for ( j = 1; j <= n; j++)
+        {
+            if (j<=n-i)
+            {
+                printf(" ");
+            }
+            else if (j>n-i)
+            {
+                printf("* ");
+            }
+        }
+        printf("\n");
+    }
+    
+}
